5b0573f08c934a0516b2397a0d06055f8850a4d2
Mazid70 <mazidurmazid431@gmail.com>
2024-07-16 18:15:06 +0600

    overview page added

--- src/DashBoard/User/Overview.jsx
+++ src/DashBoard/User/Overview.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import { FaUser } from 'react-icons/fa';
+import { MdAttachMoney } from 'react-icons/md';
 
 const Overview = () => {
   return (
-    <section className='text-white'>
-       <div>
-      <h1>User Overview</h1>
-      <p>Name: John Doe</p>
-      <p>Email: johndoe@example.com</p>
-      <p>Phone: +8801234567890</p>
-      <p>Account Balance: 500 Taka</p>
-    </div>
+    <section className="text-white self-start mt-20">
+      <div className="flex gap-10">
+        {/* one  */}
+        <div className="bg-[#0B0D27] px-5 py-10 flex items-center gap-5 rounded">
+          <div className="">
+          <img src="man.png"  className="h-14 w-14 rounded-full" alt="" />
+          </div>
+          <div>
+            <h1 className="font-bold text-2xl">Mazidur Rahman</h1>
+            <h1 className="font-semibold text-xl">+88 01639479596</h1>
+            </div>
+        </div>
+        {/* two  */}
+        <div className="bg-[#0B0D27] px-5 py-10 flex items-center gap-5 rounded">
+          <div className="bg-[#1A1F37] rounded p-2">
+            <FaUser className="text-4xl text-blue-600" />
+          </div>
+          <div>
+            <h1 className="font-bold text-lg">User Details</h1>
+            <h1 className="font-semibold">mazidurmazid43a@gamil.com</h1>
+            <h1 className="font-semibold ">Joining Date:</h1>
+            
+          </div>
+        </div>
+        {/* three  */}
+        <div className="bg-[#0B0D27] px-5 py-10 flex items-center gap-5 rounded">
+          <div className="bg-[#1A1F37] rounded ">
+            <MdAttachMoney className="text-5xl text-blue-600" />
+          </div>
+          <div><h1 className="font-bold text-2xl">Balance : 2000 Tk</h1></div>
+        </div>
+      </div>
+      
     </section>
   );
 };
--- src/Pages/Home/User/Register.jsx
+++ src/Pages/Home/User/Register.jsx
@@ -3,58 +3,88 @@ import { FaPhoneAlt, FaUser } from 'react-icons/fa';
 import { IoIosEye, IoIosEyeOff } from 'react-icons/io';
 import { MdEmail } from 'react-icons/md';
 import { RiLockPasswordFill } from 'react-icons/ri';
+import useAxiosPublic from '../../../CustomHooks/UseAxiosPublic';
+
 
 const Register = () => {
-  const [isVisible,setVisible]=useState(false);
-  const handleShowPassword=()=>{
-    setVisible(!isVisible)
-  }
+  const [isVisible, setVisible] = useState(false);
+  const axiosPublic=useAxiosPublic();
+  const handleShowPassword = () => {
+    setVisible(!isVisible);
+  };
+  
+  const handleRegister = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const pin = form.pin.value;
+    const userInfo = { name, email, phone, pin };
+    axiosPublic.post('/users',userInfo)
+  };
   return (
     <div className=" p-8 rounded bg-white bg-opacity-5">
       <h1 className="font-bold text-4xl italic text-white mb-5">Register</h1>
-      <form className="text-white w-[300px] space-y-3">
+      <form className="text-white w-[300px] space-y-3" onSubmit={handleRegister}>
         <div className="relative">
           <FaUser className="absolute text-gray-400 top-9 left-3 text-lg " />
-          
+
           <br />
           <input
             type="text"
+            name="name"
             className="h-10 w-full  pl-14  bg-transparent border-b-2 outline-0"
             placeholder="Enter Your Name"
+            required
           />
         </div>
         <div className="relative">
           <MdEmail className="absolute text-gray-400 top-9 left-3 text-lg" />
-          
+
           <br />
           <input
             type="email"
+            name="eamil"
             className="h-10 w-full pl-14 bg-transparent border-b-2 outline-0"
             placeholder="Enter Your Email"
+            required
           />
         </div>
         <div className="relative">
           <FaPhoneAlt className="absolute text-gray-400 top-9 left-3 text-lg" />
-         
+
           <br />
           <input
             type="number"
+            name="phone"
             className="h-10 w-full  pl-14 bg-transparent border-b-2 outline-0"
             placeholder="Enter Your Phone Number "
+            required
           />
         </div>
         <div className="relative">
           <RiLockPasswordFill className="absolute text-gray-400 top-9 left-3 text-lg" />
-      
+
           <br />
           <input
-            type={isVisible?"number":"password"}
+            name="pin"
+            type={isVisible ? 'number' : 'password'}
             className="h-10 w-full  pl-14 bg-transparent border-b-2 outline-0"
             placeholder="Enter Your Pin"
-            title="Minimum 5 number"
+            required
           />
-          {isVisible?<IoIosEyeOff onClick={handleShowPassword} className="absolute text-gray-400 top-9 right-3 text-xl"/>:<IoIosEye onClick={handleShowPassword}  className="absolute text-gray-400 top-9 right-3 text-xl"/>}
-          
+          {isVisible ? (
+            <IoIosEyeOff
+              onClick={handleShowPassword}
+              className="absolute text-gray-400 top-9 right-3 text-xl"
+            />
+          ) : (
+            <IoIosEye
+              onClick={handleShowPassword}
+              className="absolute text-gray-400 top-9 right-3 text-xl"
+            />
+          )}
         </div>
         <div>
           <input
